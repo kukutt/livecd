03150c5bbee74804d4bb5e091d2892f224491a88
panyao <pan3yao@gmail.com>
2020-12-30 22:27:45 +0800

    squashfs 打包完成，未验证

--- pack.sh
+++ pack.sh
@@ -8,21 +8,21 @@ function isExist(){
 	if ! type $1 >/dev/null 2>&1; then
 		echo "$1 未安装,尝试以下命令";
 		echo "sudo apt-get update";
-		echo "sudo apt-get install -y build-essential";
+		echo "sudo apt-get install -y build-essential zlib1g-dev";
 		ret=error;
 	fi
 }
 [ "ok" == $ret ] && isExist make;
 [ "ok" == $ret ] && isExist gcc;
+[ "ok" != $ret ] && exit;
 
 MKISOTOOL=$PWD/cdrtools/mkisofs/OBJ/x86_64-linux-cc/mkisofs
 function runmain(){
-	#[ -d "./isofs" ] && chmod -R 777 ./isofs && rm -rf isofs
+	[ -d "./isofs" ] && chmod -R 777 ./isofs && rm -rf isofs
 	[ -f "./test.iso" ] && rm ./test.iso
-	#tar -zxf isofs.tgz
-	#cp filesystem.squashfs isofs/casper/ 
-	#$MKISOTOOL -iso-level 3 -r -V sblive -cache-inodes -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -c isolinux/boot.cat -o test.iso isofs
-	$MKISOTOOL -iso-level 3 -r -V sblive -cache-inodes -J -l -b syslinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -c syslinux/boot.cat -o test.iso isofs
+	tar -zxf isofs.tgz
+	cp filesystem.squashfs isofs/casper/ 
+	$MKISOTOOL -iso-level 3 -r -V sblive -cache-inodes -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -c isolinux/boot.cat -o test.iso isofs
 }
 
 runmain
