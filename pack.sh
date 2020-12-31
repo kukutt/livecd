425d734a444aef9f9553ac286e01d5d6c62cfff4
panyao <pan3yao@gmail.com>
2020-12-31 15:17:28 +0800

    编译tools添加

--- pack.sh
+++ pack.sh
@@ -2,44 +2,65 @@
 #
 # author: pan3yao@gmail.com
 #
-function root_need(){
-    if [[ $EUID -ne 0 ]]; then
-        echo "需要root权限" 1>&2
-        exit 1
-    fi
-}
-ret=ok
-# 检查系统工具是否安装
-function isExist(){
-	if ! type $1 >/dev/null 2>&1; then
-		echo "$1 未安装,尝试以下命令";
-		echo "sudo apt-get update";
-		echo "sudo apt-get install -y build-essential zlib1g-dev";
-		ret=error;
+MKISOTOOL=$PWD/cdrtools-3.02/mkisofs/OBJ/x86_64-linux-cc/mkisofs
+MKSQUASHFS=$PWD/squashfs4.3/squashfs-tools/mksquashfs
+
+function precheck(){
+	if [[ $EUID -ne 0 ]]; then
+		echo "需要root权限" 1>&2
+		exit 1
+	fi
+
+	insflg=no
+	if [ ! -d "squashfs4.3" ]; then
+		[ "ok" != isnflg ] && apt-get update && apt-get install -y build-essential zlib1g-dev && insflg=ok
+		tar -zxf tools/squashfs4.3.tar.gz
+		pushd squashfs4.3/squashfs-tools/
+		make
+		popd
+	fi
+	
+	if [ ! -d "cdrtools-3.02" ]; then
+		[ "ok" != isnflg ] && apt-get update && apt-get install -y build-essential zlib1g-dev && insflg=ok
+		tar -zxf tools/cdrtools-3.02a07.tar.gz
+		pushd cdrtools-3.02
+		make
+		popd
 	fi
 }
-[ "ok" == $ret ] && isExist make;
-[ "ok" == $ret ] && isExist gcc;
-[ "ok" != $ret ] && exit;
 
-MKISOTOOL=$PWD/cdrtools/mkisofs/OBJ/x86_64-linux-cc/mkisofs
-MKSQUASHFS=$PWD/squashfs/squashfs-tools/mksquashfs
 function mkrootfs(){
 	[ -d "./squashfs-root" ] && rm -rf squashfs-root
 	[ -f "./filesystem.squashfs" ] && rm ./filesystem.squashfs
 	mkdir squashfs-root
-	cp -raf squashfs-root-bk/* squashfs-root/
+	mount | awk '{print $3}' | grep "snap" > exfile
+	rsync -av --exclude=$PWD --exclude=/tmp \
+		--exclude=/media \
+		--exclude=/dev \
+		--exclude=/proc \
+		--exclude=/run \
+		--exclude=/sys \
+		--exclude=/lost+found \
+		--exclude=/swapfile \
+		--exclude-from=./exfile \
+		/ ./squashfs-root/
+	mkdir -m 755 squashfs-root/media
+	mkdir -m 755 squashfs-root/dev
+	mkdir -m 555 squashfs-root/proc
+	mkdir -m 755 squashfs-root/run
+	mkdir -m 555 squashfs-root/sys
+	mkdir -m 1777 squashfs-root/tmp
 	$MKSQUASHFS squashfs-root/ ./filesystem.squashfs -comp gzip -b 1024k -always-use-fragments -no-duplicates
 }
 
 function geniso(){
 	[ -d "./isofs" ] && rm -rf isofs
 	[ -f "./test.iso" ] && rm ./test.iso
-	tar -zxf isofs.tgz
+	tar -zxf tools/isofs.tgz
 	cp filesystem.squashfs isofs/casper/ 
 	$MKISOTOOL -iso-level 3 -r -V sblive -cache-inodes -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -c isolinux/boot.cat -o test.iso isofs
 }
 
-root_need
+precheck
 mkrootfs
 geniso
